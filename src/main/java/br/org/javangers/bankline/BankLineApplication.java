523d89a66c838204bbe156cfd843ef1fcee9e1e2
ianmichael <ianmichael.xp@gmail.com>
2021-03-01 20:43:37 -0300

    API bankline

---
+++ src/main/java/br/org/javangers/bankline/BankLineApplication.java
@@ -0,0 +1,13 @@
+package br.org.javangers.bankline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankLineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankLineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/org/javangers/bankline/BankLineApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.javangers.bankline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankLineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
