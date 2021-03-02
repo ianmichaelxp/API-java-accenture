d6b963d49001811ea16e3c2c5954ec526d569049
ianmichael <ianmichael.xp@gmail.com>
2021-03-02 17:26:34 -0300

    Criação da lógica de conversão de Entity to DTO

--- src/main/java/br/org/javangers/bankline/controller/UsuarioController.java
+++ src/main/java/br/org/javangers/bankline/controller/UsuarioController.java
@@ -1,13 +1,16 @@
 package br.org.javangers.bankline.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.org.javangers.bankline.controller.dto.UsuarioDTO;
 import br.org.javangers.bankline.model.Usuario;
 import br.org.javangers.bankline.service.UsuarioService;
 
@@ -19,9 +22,20 @@ public class UsuarioController {
 	private UsuarioService usuarioService;
 
 	@GetMapping
-	@ResponseBody
-	public List<Usuario> getListaUsuarios() {
-		List<Usuario> usuarios = usuarioService.listarUsuarios();
-		return usuarios;
+	public List<UsuarioDTO> obterListaUsuarios() {
+		List<UsuarioDTO> usuariosDTO = usuarioService.listarUsuarios();
+		return usuariosDTO;
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<UsuarioDTO> getUsuario(@PathVariable(value="id") Long id) {
+		
+		UsuarioDTO usuarioDTOs = usuarioService.obterPorId(id);
+		if (usuarioDTOs != null) {
+			return ResponseEntity.ok().body(usuarioDTOs);
+		} else {
+			//ref
+			return ResponseEntity.notFound().build();
+		}
 	}
 }
--- src/main/java/br/org/javangers/bankline/controller/dto/UsuarioDTO.java
+++ src/main/java/br/org/javangers/bankline/controller/dto/UsuarioDTO.java
@@ -1,15 +1,22 @@
 package br.org.javangers.bankline.controller.dto;
 
-import lombok.AllArgsConstructor;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import br.org.javangers.bankline.model.Usuario;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Setter
 public class UsuarioDTO {
+
+	public UsuarioDTO(Usuario usuario) {
+		this.login = usuario.getLogin();
+		this.senha = usuario.getSenha();
+		this.nome = usuario.getNome();
+		this.cpf = usuario.getCpf();
+	}
 
 	private String login;
 	
@@ -18,5 +25,14 @@ public class UsuarioDTO {
 	private String nome;
 	
 	private String cpf;
+	
+	public static List<UsuarioDTO> usuarioToDTO(List<Usuario> usuarios) {
+	    List<UsuarioDTO> UsuariosDTOs = usuarios.stream().map(usuario -> {
+	        return new UsuarioDTO(usuario);
+	    }).collect(Collectors.toList());
+
+	    return UsuariosDTOs;
+	}
+	
 	
 }
