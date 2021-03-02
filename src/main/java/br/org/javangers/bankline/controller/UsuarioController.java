0bd91223b2065e35cdbe869e647cc91030b62d2f
ianmichael <ianmichael.xp@gmail.com>
2021-03-02 11:36:04 -0300

    UsuarioController

--- src/main/java/br/org/javangers/bankline/controller/UsuarioController.java
+++ src/main/java/br/org/javangers/bankline/controller/UsuarioController.java
@@ -1,5 +1,27 @@
 package br.org.javangers.bankline.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.org.javangers.bankline.model.Usuario;
+import br.org.javangers.bankline.service.UsuarioService;
+
+@RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
 
+	@Autowired 
+	private UsuarioService usuarioService;
+
+	@GetMapping
+	@ResponseBody
+	public List<Usuario> getListaUsuarios() {
+		List<Usuario> usuarios = usuarioService.listarUsuarios();
+		return usuarios;
+	}
 }
