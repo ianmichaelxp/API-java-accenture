15a8a00aa31d98636defe041d8da70c1d84d13fd
ianmichael <ianmichael.xp@gmail.com>
2021-03-01 21:16:37 -0300

    Model usuario data

--- src/main/java/br/org/javangers/bankline/model/Usuario.java
+++ src/main/java/br/org/javangers/bankline/model/Usuario.java
@@ -1,5 +1,35 @@
 package br.org.javangers.bankline.model;
 
-public class Usuario {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Entity
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NonNull
+	private String login;
+	
+	@NonNull
+	private String senha;
+	
+	@NonNull
+	private String nome;
+	
+	@NonNull
+	private String cpf;
+	
 }
