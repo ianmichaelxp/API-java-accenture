3a793ae6c2dfe1fe5bc3f3c860b5514e251da43d
ianmichael <ianmichael.xp@gmail.com>
2021-03-03 12:01:31 -0300

    Configurações do spring security e geração de token com JWT

---
+++ src/main/java/br/org/javangers/bankline/controller/LoginController.java
@@ -0,0 +1,46 @@
+package br.org.javangers.bankline.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.org.javangers.bankline.config.security.TokenService;
+import br.org.javangers.bankline.controller.dto.LoginDTO;
+
+@RestController
+@RequestMapping("/auth")
+public class LoginController {
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	@Autowired
+	private TokenService tokenService;
+	
+	@PostMapping
+	public ResponseEntity<?> autenticar(@RequestBody @Validated LoginDTO loginDTO ){
+		
+		UsernamePasswordAuthenticationToken dadosLogin = loginDTO.converter();
+		try {
+			Authentication authenticate = authenticationManager.authenticate(dadosLogin);
+			String token = tokenService.gerarToken(authenticate);
+			System.out.println(token);
+			
+			
+			return ResponseEntity.ok().build();
+			
+		} catch (AuthenticationException e) {
+			return ResponseEntity.badRequest().build();
+		}
+		
+	}
+
+}
